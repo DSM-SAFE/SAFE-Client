8d98c9e31cb818222b5520a9871a4ec9f9e102ba
rlarldud1234 <rlarldud6482@naver.com>
2023-04-11 10:38:04 +0900

    feat :: SFillButton

---
+++ src/components/button/SFillButton.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {StyleSheet, Text} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+
+interface Props {
+  title: string;
+  backgroundColor: string;
+  tintColor: string;
+  onPress: () => void;
+}
+
+const SFillButton = (props: Props) => {
+  return (
+    <TouchableOpacity
+      style={[styles.button, {backgroundColor: props.backgroundColor}]}
+      onPress={props.onPress}>
+      <Text style={[styles.title, {color: props.tintColor}]}>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+    height: 68,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+});
+
+export default SFillButton;
