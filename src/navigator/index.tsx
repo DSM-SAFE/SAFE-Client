101029e45a99696287b2f5417cbb5b86825b0e63
rlarldud1234 <rlarldud6482@naver.com>
2023-04-13 13:57:42 +0900

    feat :: navigator

---
+++ src/navigator/index.tsx
@@ -0,0 +1,69 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import React from 'react';
+import {OnboardingScreen} from '../screen/onboarding';
+import {LoginScreen} from '../screen/login';
+import {SignUpScreen} from '../screen/signup';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export enum RootScreens {
+  Onboarding = 'Onboarding',
+  Login = 'Login',
+  SignUp = 'SignUp',
+}
+
+export type RootStackList = {
+  Onboarding: undefined;
+  Login: undefined;
+  SignUp: undefined;
+};
+
+const Stack = createStackNavigator<RootStackList>();
+
+const RootStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName={RootScreens.Onboarding}
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#F5F4F4',
+          },
+          headerBackTitleVisible: false,
+          headerTintColor: '#000',
+          headerBackImage: props => {
+            return (
+              <Icon
+                style={{marginLeft: 16}}
+                name="arrow-back-outline"
+                color={props.tintColor}
+                size={19}
+              />
+            );
+          },
+        }}>
+        <Stack.Screen
+          name={RootScreens.Onboarding}
+          component={OnboardingScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name={RootScreens.Login}
+          component={LoginScreen}
+          options={{
+            title: '로그인',
+          }}
+        />
+        <Stack.Screen
+          name={RootScreens.SignUp}
+          component={SignUpScreen}
+          options={{
+            title: '회원가입',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default RootStack;
