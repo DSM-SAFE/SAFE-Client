f05145e09b9e60c0dbbadf0056532dff5bfe6d02
rlarldud1234 <rlarldud6482@naver.com>
2023-05-26 12:06:34 +0900

    feat :: CommunityScreen

--- src/screen/community/CommunityScreen.tsx
+++ src/screen/community/CommunityScreen.tsx
@@ -1,8 +1,41 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {ScrollView} from 'react-native-gesture-handler';
+import {CommunityListItem} from '../../components/community';
+
+interface Community {
+  title: string;
+}
 
 const CommunityScreen = () => {
-  return <Text>Community</Text>;
+  const [dummy, setDummy] = useState<Community[]>([]);
+
+  useEffect(() => {
+    setDummy([
+      {
+        title: '반가워요',
+      },
+      {
+        title: '안녕하세요',
+      },
+      {
+        title: '정말 신기해요!',
+      },
+      {
+        title: '그냥 싶어요!',
+      },
+    ]);
+  }, []);
+
+  return (
+    <ScrollView>
+      {dummy.map((item: Community) => {
+        const {title} = item;
+        return (
+          <CommunityListItem title={title} onPress={() => console.log(title)} />
+        );
+      })}
+    </ScrollView>
+  );
 };
 
 export default CommunityScreen;
