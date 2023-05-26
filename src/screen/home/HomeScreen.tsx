58f26c7d9252fad5b4b30c5f9082fad888ec5e12
rlarldud1234 <rlarldud6482@naver.com>
2023-05-26 12:06:41 +0900

    feat :: HomeScreen

--- src/screen/home/HomeScreen.tsx
+++ src/screen/home/HomeScreen.tsx
@@ -1,8 +1,59 @@
-import React from 'react';
-import {Text} from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, Text} from 'react-native';
+import MapView from 'react-native-maps';
+
+interface Location {
+  latitude: number;
+  longitude: number;
+}
 
 const HomeScreen = () => {
-  return <Text>Home</Text>;
+  const [location, setLocation] = useState<Location | undefined>(undefined);
+
+  useEffect(() => {
+    const _watchId = Geolocation.watchPosition(
+      position => {
+        const {latitude, longitude} = position.coords;
+        setLocation({latitude, longitude});
+      },
+      error => {
+        console.log(error);
+      },
+      {
+        enableHighAccuracy: true,
+        distanceFilter: 0,
+        interval: 5000,
+        fastestInterval: 2000,
+      },
+    );
+
+    return () => {
+      if (!_watchId) {
+        Geolocation.clearWatch(_watchId);
+      }
+    };
+  });
+
+  return location ? (
+    <MapView
+      style={styles.map}
+      initialRegion={{
+        latitude: location?.latitude!,
+        longitude: location?.longitude!,
+        latitudeDelta: 0.007234,
+        longitudeDelta: 0.007234,
+      }}
+    />
+  ) : (
+    <Text>...Loading</Text>
+  );
 };
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+  },
+});
 
 export default HomeScreen;
