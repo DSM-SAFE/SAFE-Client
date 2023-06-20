408c361471957e733bf1e1075ed09b1302afe812
rlarldud1234 <rlarldud6482@naver.com>
2023-06-20 09:16:36 +0900

    feat :: ImagePicker

--- src/components/imagePicker/ImagePicker.tsx
+++ src/components/imagePicker/ImagePicker.tsx
@@ -1,33 +1,39 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Image, StyleSheet, TouchableOpacity} from 'react-native';
 import {launchImageLibrary} from 'react-native-image-picker';
 
-const SImagePicker = () => {
-  const [uri, setUri] = useState<string>('');
-
+interface Props {
+  uri: string;
+  setUri: (uri: string) => void;
+}
+const ImagePicker = (props: Props) => {
   const pickImage = async () => {
     await launchImageLibrary({mediaType: 'photo'}, response => {
       if (response.assets) {
         const url = response.assets[0].uri;
-        setUri(url!);
+        props.setUri(url!);
       }
     });
   };
 
   return (
-    <TouchableOpacity onPress={pickImage}>
-      <Image style={styles.image} source={{uri: uri}} />
+    <TouchableOpacity style={styles.button} onPress={pickImage}>
+      <Image style={styles.image} source={{uri: props.uri}} />
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
+  button: {
+    alignSelf: 'center',
+  },
   image: {
     backgroundColor: 'gray',
     width: 100,
     height: 100,
     borderRadius: 50,
+    marginBottom: 34,
   },
 });
 
-export default SImagePicker;
+export default ImagePicker;
